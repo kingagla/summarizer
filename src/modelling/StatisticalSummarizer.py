450991baf4b2f593040811f79987ff463dbe3736
Kinga Głąbińska <kingagla@gmail.com>
2021-10-07 19:39:24 +0200

    improve sentence splitter

--- src/modelling/StatisticalSummarizer.py
+++ src/modelling/StatisticalSummarizer.py
@@ -8,6 +8,9 @@ stopwords = nlp.Defaults.stop_words
 
 
 class StatisticalSummarizer:
+
+    new_sentence_splitter = '\. (?=[A-ZĆŁŃÓŚŹŻ])|\n-'
+
     def __init__(self, article):
         self.title = article['title']
         self.text = article['text']
@@ -15,9 +18,14 @@ class StatisticalSummarizer:
 
     @property
     def lemmatized_article(self):
-        text = spacy_lemmatize(self.text.lower())
-        text = " ".join([word for word in text.split() if word not in stopwords])
-        return re.sub('[^a-ż\.]', ' ', text)
+        text = re.split(self.new_sentence_splitter, self.text)
+        new_text = ''
+        for sentence in text:
+            temp_text = spacy_lemmatize(sentence)
+            temp_text = re.sub('[^a-ż]', ' ', temp_text)
+            " ".join([word for word in temp_text.split() if word not in stopwords])
+            new_text += spacy_lemmatize(sentence) + '. '
+        return new_text
 
     @property
     def common_words(self):
@@ -40,5 +48,6 @@ class StatisticalSummarizer:
     def create_summary(self, n_sentences=3):
         sentences_to_summary = sorted(self._text_to_score(), key=lambda x: x[1], reverse=True)
         sentences_to_summary = list(map(lambda x: x[0], sentences_to_summary))[:n_sentences]
-        sentences_to_summary = map(self.text.split('.').__getitem__, sentences_to_summary)
-        return ".".join(sentences_to_summary)
+        split_text = re.split(self.new_sentence_splitter, self.text)
+        sentences_to_summary = map(split_text.__getitem__, sentences_to_summary)
+        return ". ".join(sentences_to_summary)
